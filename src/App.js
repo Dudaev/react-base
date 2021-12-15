16fd35d103effb5a6f84f1af87c1a259fd74acb3
Dudaev Nikita <dudaev.nik@gmail.com>
2021-12-15 22:30:00 +0600

    Функциональный и классовый компонент

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Counter from "./components/Counter.jsx";
+import ClassCounter from "./components/ClassCounter.jsx";
+
 function App() {
   return (
     <div className="App">
-     
+      <Counter />
+      <ClassCounter />
     </div>
   );
 }
